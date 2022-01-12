9f4a3f4e72e4fefad1952f2548acfb61ba3d7d9a
Casey_Nelson <91818705+CaseyNelson314@users.noreply.github.com>
2022-01-13 00:19:22 +0900

    Update LEDillumination.ino

--- LEDillumination/LEDillumination.ino
+++ LEDillumination/LEDillumination.ino
@@ -1,29 +1,10 @@
-#define I2C_ADDRESS 1
-#define DATA_SIZE   1
-
-#include <Wire.h>
-
 #include "SerialLedController.h"
 LedController led(13, 100, 1000, 50);
 //遅延0.7ms/led
 
-int8_t buf[DATA_SIZE];
-
 void setup() {
-  delay(100);
-  Wire.begin(I2C_ADDRESS);
-  Wire.onReceive(receiveEvent);
-  Serial.begin(115200);
 }
 
 void loop() {
-  long owTime = micros();
   led.showWave();
-  Serial.println(micros() - owTime);
-}
-
-void receiveEvent() {
-  while (Wire.available())
-    for (int i = 0; i < DATA_SIZE; i++)
-      buf[i] = Wire.read();
 }
